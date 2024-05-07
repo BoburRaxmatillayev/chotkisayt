f411d874884cfd7d855f57d73a657fc0a285247a
BoburRaxmatillayev <boburraxmatillayev2201@gmail.com>
2024-05-07 20:49:55 +0500

    first commit

---
+++ chotkinarsa/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ChotkinarsaConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'chotkinarsa'
---
+++ chotkinarsa/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from django.contrib import messages
+
+def home_view(request):
+    return render(request, "index.html")
