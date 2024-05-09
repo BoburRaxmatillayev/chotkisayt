f647c2ab1d89910cfd0d64beb7613acdb8c92f22
BoburRaxmatillayev <boburraxmatillayev2201@gmail.com>
2024-05-09 18:41:52 +0500

    first commit

--- index.js
+++ index.js
@@ -237,7 +237,7 @@ function generateAirPlane() {
     flatShading: true,
     fog: false,
   });
-  const flagText = new THREE.TextureLoader().load("./peace-flag.jpg");
+  const flagText = new THREE.TextureLoader().load("{% static 'images  /peace-flag.jpg' %}");
   const matFlag = new THREE.MeshStandardMaterial({
     map: flagText,
     roughness: 0.5,
@@ -827,21 +827,21 @@ generateAirPlane();
 projectResize()
 animate();
 
-document.addEventListener("DOMContentLoaded", function() {
-  // Telefon raqamini o'qish
-  const phoneNumberElement = document.querySelector('.phoneNumber');
-  // "Send message" tugmasini olish
-  const sendMessageButton = document.querySelector('.secMail');
+// document.addEventListener("DOMContentLoaded", function() {
+//   // Telefon raqamini o'qish
+//   const phoneNumberElement = document.querySelector('.phoneNumber');
+//   // "Send message" tugmasini olish
+//   const sendMessageButton = document.querySelector('.secMail');
 
-  if (phoneNumberElement && sendMessageButton) {
-      // "Send message" tugmasini bosganda
-      sendMessageButton.addEventListener('click', function(event) {
-          // `phoneNumberElement` elementidagi telefon raqamini oling
-          const phoneNumber = phoneNumberElement.textContent;
+//   if (phoneNumberElement && sendMessageButton) {
+//       // "Send message" tugmasini bosganda
+//       sendMessageButton.addEventListener('click', function(event) {
+//           // `phoneNumberElement` elementidagi telefon raqamini oling
+//           const phoneNumber = phoneNumberElement.textContent;
           
-          // Matn yozish joyiga telefon raqamini ko'chirish
-          const mailToHref = `mailto:boburraxmatillayev2201@gmail.com?subject=Message&body=Hello, I want to talk about the phone number: ${phoneNumber}`;
-          sendMessageButton.setAttribute('href', mailToHref);
-      });
-  }
-});
+//           // Matn yozish joyiga telefon raqamini ko'chirish
+//           const mailToHref = `mailto:boburraxmatillayev2201@gmail.com?subject=Message&body=Hello, I want to talk about the phone number: ${phoneNumber}`;
+//           sendMessageButton.setAttribute('href', mailToHref);
+//       });
+//   }
+// });
